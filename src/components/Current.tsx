d81eb86bfc6389c2618332a2ad60dd2fa8788981
Mahmoud-Moghazy <ma.moghazy@gmail.com>
2024-10-29 12:25:31 +0300

    fix time iss on iphone

--- src/components/Current.tsx
+++ src/components/Current.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { WeatherData } from "../constans/interfaces";
 
 const Current: React.FC<WeatherData> = ({ forecast, location }) => {
-  console.log(forecast)
-  console.log(location)
-  const avgtemp_c = Number(forecast?.forecastday[0].day?.avgtemp_c);
-  const maxtemp_c = Number(forecast?.forecastday[0]?.day?.maxtemp_c);
-  const mintemp_c = Number(forecast?.forecastday[0]?.day?.mintemp_c);
-  const condition = forecast?.forecastday[0]?.day?.condition.text;
-  const icon = forecast?.forecastday[0]?.day?.condition.icon;
-  const maxtemp_f = Number(forecast?.forecastday[0]?.day?.maxtemp_f);
-  const mintemp_f = Number(forecast?.forecastday[0]?.day?.mintemp_f);
+  const day = forecast?.forecastday[0].day;
+  const avgtemp_c = Number(day?.avgtemp_c);
+  const maxtemp_c = Number(day?.maxtemp_c);
+  const mintemp_c = Number(day?.mintemp_c);
+  const condition = day?.condition.text;
+  const icon = day?.condition.icon;
+  const maxtemp_f = Number(day?.maxtemp_f);
+  const mintemp_f = Number(day?.mintemp_f);
 
   return (
     <section className="text-white p-5 text-center drop-shadow-lg">
@@ -18,7 +17,9 @@ const Current: React.FC<WeatherData> = ({ forecast, location }) => {
         {location?.name}, {location?.region}, {location?.country}
       </h2>
       <span className="drop-shadow-lg">
-        {new Date(location?.localtime || new Date).toLocaleString("en-US", {
+        {new Date(
+          (location?.localtime || "").replace(" ", "T") || new Date()
+        ).toLocaleString("en-US", {
           hour: "numeric",
           minute: "numeric",
           day: "numeric",
